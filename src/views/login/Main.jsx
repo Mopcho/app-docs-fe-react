91222899e7fb7ade95baf65474935ad30a71d12d
Mopcho <valeri_petkov01@abv.bg>
2022-12-14 10:58:13 +0200

    Fixed mispelling

--- src/views/login/Main.jsx
+++ src/views/login/Main.jsx
@@ -4,7 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { ErrorMessage } from "../../components/ErrorMessage/errorMessage";
+import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
 import { SuccessfulOperation } from "../../components/SuccessfulOperation/SuccessfulOperation";
 import useService from "../../service";
 
