4a280753c2322f9ca3a843c080440621803c94b6
Mopcho <valeri_petkov01@abv.bg>
2022-12-18 13:55:13 +0200

    Update Profile.jsx

--- src/views/ProfilePage/Profile.jsx
+++ src/views/ProfilePage/Profile.jsx
@@ -1,9 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import useService from "../../service";
+
 export const ProfilePage = () => {
+    const {service} = useService();
+    const navigate = useNavigate();
+    const {data, isLoading} = useQuery(['user'],async() => {
+        const response = await service.find("auth/me");
+    
+        if(response.status === 403) {
+          navigate('/login');
+        }
+    
+        return response.data;
+      });
     return (
         <section>
-            <div>
-                <h1>asdasdasdsad</h1>
-            </div>
+            <h1 className="text-4xl text-center my-10">Under Construction...</h1>
         </section>
     )
 }
